c90d60fbda3956ccf1de74566b7a5f767a35d20d
Ivan Inozemtsev <Inozemtsevco@gmail.com>
2025-11-15 13:23:44 +0100

    Initial Commit

---
+++ Chrono/ChronoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoApp.swift
+//  Chrono
+//
+//  Created by Ivan on 15.11.25.
+//
+
+import SwiftUI
+
+@main
+struct ChronoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
